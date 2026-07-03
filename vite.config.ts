a070793a17f3b2430ecfb6c232d9fde19428ea8e
katarahmat <rahmat.apriono36@guru.sma.belajar.id>
2026-07-03 15:25:21 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/inventaris-lab-sman1/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
